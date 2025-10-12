1fdfc2044357554816d020ebe39b0b42ee0a1a46
Aaditya03k <220701003@rajalakshmi.edu.in>
2025-10-12 18:47:24 +0530

    Update index.js

--- classroom-booking/src/index.js
+++ classroom-booking/src/index.js
@@ -11,12 +11,6 @@ app.use(express.json()); // 👈 Needed to parse JSON body
 // routes
 import authRoutes from "./routes/authRoutes.js";
 app.use("/api/auth", authRoutes);
-app.use(express.static(path.join(__dirname, 'classroom-booking/build')));
-
-// Handle all GET requests by sending back React's index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'classroom-booking/build', 'index.html'));
-});
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server running on port", process.env.PORT || 5000);
